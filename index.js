a3723d5bc1a70a7888fad6847d3a6e7a1c578a13
Lusuzucky <david990120@126.com>
2023-02-07 14:23:34 +0800

    顺序加载

--- index.js
+++ index.js
@@ -1,63 +1,21 @@
-// const images = document.querySelectorAll('img')
+//获取页面所有图片
+var images = document.images;
+console.log(images);
+//图片异步加载
+preload(images,0);
 
-// const callback = entries => {
-//   entries.forEach(entry => {
-//     if (entry.isIntersecting) {
-//       const image = entry.target;
-//       const data_src = image.getAttribute('data-src');
-//       image.setAttribute('src', data_src);
-//       observer.unobserve(image);
-//       console.log('触发');
-//     };
-//   });
-// };
-
-// const observer = new IntersectionObserver(callback);
-
-// images.forEach(image => {
-//   observer.observe(image);
-// });
-
-
-// 创建一个数组，存放要加载的图片src
-var src = new Array();
-		src.push('img/01.jpg');
-    src.push('img/02.jpg');
-    src.push('img/03.jpg');
-		src.push('img/04.jpg');
-		src.push('img/05.jpg');
-		src.push('img/06.jpg');
-		src.push('img/08.jpg');
-		src.push('img/09.jpg');
-		src.push('img/07.jpg');
-		src.push('img/10_1.jpg');
-		src.push('img/10_2.jpg');
-		src.push('img/10_3.jpg');
-		src.push('img/10_4.jpg');
-		src.push('img/10_5.jpg');
-		src.push('img/10_6.jpg');
-		src.push('img/11.jpg');
-		src.push('img/12_1.jpg');
-		src.push('img/12_2.jpg');
-		src.push('img/12_3.jpg');
-		src.push('img/13.jpg');
-		src.push('img/01.jpg');
-
-
-// 图片按顺序加载，使用递归算法，直到加载完最后一张图片，退出递归
-function preload(src, index) {
-	index = index || 0;
-	if(index >= 21) {
-		return false;
-	}
-	if (src && src.length > index) {
-		var img = new Image();
-		img.onload = function() {
-				preload(src, index + 1);
-		}
-		img.src = src[index];
-	}
+//图片异步加载
+function preload(images, index) {
+    index = index || 0;
+    if (images && images.length > index) {
+        var img = images[index];
+		console.log(img);
+        //图片加载完成后回到函数中开始加载下一张图片
+        img.onload = function() {
+			console.log(index)
+            preload(images, index + 1);
+        };
+        var src = img.getAttribute('data-src');
+        img.src = src;
+    }
 }
-preload(src);
-
-
